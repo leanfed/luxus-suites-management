784ccb6595098d613fe91b588b9df6e8a48f2176
Federico Sosa <lfedericososa@hotmail.com>
2026-07-04 22:33:57 -0300

    Ordenar reservas recientes en admin

--- src/main/java/com/luxus/suites/repository/ReservaSolicitudRepository.java
+++ src/main/java/com/luxus/suites/repository/ReservaSolicitudRepository.java
@@ -3,5 +3,9 @@ package com.luxus.suites.repository;
 import com.luxus.suites.model.ReservaSolicitud;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservaSolicitudRepository extends JpaRepository<ReservaSolicitud, Long> {
+
+    List<ReservaSolicitud> findAllByOrderByIdDesc();
 }
--- src/main/java/com/luxus/suites/service/ReservaService.java
+++ src/main/java/com/luxus/suites/service/ReservaService.java
@@ -66,7 +66,7 @@ public class ReservaService {
     }
 
     public List<ReservaSolicitud> listarSolicitudes() {
-        return reservaSolicitudRepository.findAll();
+        return reservaSolicitudRepository.findAllByOrderByIdDesc();
     }
 
     public ReservaSolicitud buscarPorId(Long id) {
